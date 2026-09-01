f94bdd35ea387e64bb07c3534218ec4d674f7e76
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-09-01 23:48:41 +0530

    name add

--- EventLisner/EventLisnerAlltopic.js
+++ EventLisner/EventLisnerAlltopic.js
@@ -103,6 +103,8 @@ form.addEventListener("submit", function(dets){
       console.log("gautam");
       console.log("gautam");
       console.log("gautam");
+      console.log("Rk..");
+      
 v
 v
 
